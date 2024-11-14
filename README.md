52c5e141e531018d3c693ee6260775e60ddf8494
Eduardo Saavedra Perafán <saavedra.dev@gmail.com>
2024-11-14 17:03:49 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## Curso: Despliega tu lambda en AWS usando Github actions - 2024
 ## Link: https://www.youtube.com/watch?v=N_7lO4oyg0c
 
+## Iniciar proyecto node
+npm init -y
+npm i -save-dev @vercel/ncc
---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+  console.log('Event: ', event);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+
+  return response;
+};
